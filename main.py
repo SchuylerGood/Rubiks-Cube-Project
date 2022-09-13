3ead73aa9b583025250aa0f29dc074da6b7d5184
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2022-09-12 20:26:46 -0400

    added more functions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# Rubiks Cube Solver
-
+"""
+Program:        Rubiks Cube Solver
+Contributors:   Sky, Akshay, Dylan, Eric
+Date:           9/12/2022
+Course:         CISC 204 - Final Logic Project
+"""
 
 class Cube:
     def __init__(self, sides):
@@ -8,26 +12,67 @@ class Cube:
 CORESPONDING_SIDES_INT = {0:"Top",1:"Bottom",2:"Front",3:"Back",4:"Left",5:"Right"}
 CORESPONDING_SIDES_STRING = {"Top":0,"Bottom":1,"Front":2,"Back":3,"Left":4,"Right":5}
 
-
 def findWhiteCenter(cube):
-    if(type(cube) == Cube):
-        for i in range(len(cube.sides)):
-            if cube.sides[i][1][1] == "w":
-                print('White Center:', CORESPONDING_SIDES_INT[i]) # displays where the white center is
+    """
+    Function:   Finds where the white center is on the cube
+    Parameters: cube (of type Cube)
+    Returns:    String - what side of the cube the white center is on
+    """
+    for i in range(len(cube.sides)):
+        if cube.sides[i][1][1] == "w":
+            return CORESPONDING_SIDES_INT[i]
 
-# Function:     Determins if a side is solved
-# Parameters:   cube - type cube
-#               side - type String (corresponds to CORRESPONDING_SIDES global variable)
-# Returns:      Boolean value 
+
+def findWhiteMiddles(cube):
+    """
+    Function:   Finds the locations of all the white-middle squares
+    Parameters: cube (of type Cube)
+    Returns:    2D list of the 4 locations of the white-middle squares 
+    """
+    locations = []
+    for i in range(len(cube.sides)):
+        if(cube.sides[i][0][1] == "w"):
+            locations.append([i,0,1])
+        if(cube.sides[i][1][0] == "w"):
+            locations.append([i,1,0])
+        if(cube.sides[i][1][2] == "w"):
+            locations.append([i,1,2])
+        if(cube.sides[i][2][1] == "w"):
+            locations.append([i,2,1])
+
+    return locations
+
+
 def isSideSolved(cube, side):
+    """
+    Function:   Determins if a side is solved
+    Parameters: cube - type cube
+                side - type String (corresponds to CORRESPONDING_SIDES global variable)
+    Returns:    Boolean value 
+    """
     sideInt = CORESPONDING_SIDES_STRING[side]
-    if(type(cube) == Cube):
-        for i in range(2):
-            for j in range(len(cube.sides[sideInt][i])):
-                sideColor = cube.sides[sideInt][1][1]
-                if cube.sides[sideInt][i][j] != sideColor:
-                    return False
-        return True
+    for i in range(2):
+        for j in range(len(cube.sides[sideInt][i])):
+            sideColor = cube.sides[sideInt][1][1]
+            if cube.sides[sideInt][i][j] != sideColor:
+                return False
+    return True
+
+
+def isCubeSolved(cube):
+    """
+    Function:   Determines if the cube is solved
+    Parameters: cube of type Cube
+    Returns:    Boolean value
+    """
+    sideChecks = []
+    for i in range(len(cube.sides)):
+        sideChecks.append(isSideSolved(cube, CORESPONDING_SIDES_INT[i]))
+    for i in sideChecks:
+        if(i != True):
+            return False
+        else:
+            return True
 
 
 def main():
@@ -39,11 +84,12 @@ def main():
                  [["r","r","r"],["r","r","r"],["r","r","r"]], # Left
                  [["o","o","o"],["o","o","o"],["o","o","o"]]] # Right
     )
-
-
-    findWhiteCenter(cube1)
+    print('White Center:', findWhiteCenter(cube1))
     print(isSideSolved(cube1, "Front"))
+    print(findWhiteMiddles(cube1))
+    print(isCubeSolved(cube1))
 
+    
 main()
     
     
